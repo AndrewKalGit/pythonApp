2a3a4875e250a6895b401f2aecdaa9f3a1c4983b
Andrew Kal <andrewkalgit@gmail.com>
2021-12-17 13:52:19 -0500

    2nd

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        text = request.form.get("review")
+        sia = SentimentIntensityAnalyzer()
+        quantText = sia.polarity_scores(text)
+        return quantText
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
